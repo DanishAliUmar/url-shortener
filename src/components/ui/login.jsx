6076f30e042b80dddf67460fd2076e816402d005
DanishAliUmar <danishaliumar4@gmail.com>
2024-09-24 23:59:15 +0500

    revert login code ...

--- src/components/ui/login.jsx
+++ src/components/ui/login.jsx
@@ -67,7 +67,38 @@ const Login = () => {
 
 
   return (
-    <></>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          to your account if you already have one
+        </CardDescription>
+        {errors && <Error message={errors.message} />}
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter Email"
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.email && <Error message={errors.email} />}
+        <div className="space-y-1">
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter Password"
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.password && <Error message={errors.password} />}
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleLogin}>{loading ? <BeatLoader size={10} color='#36d7b7' /> : "Login"}</Button>
+      </CardFooter>
+    </Card>
   )
 }
 
